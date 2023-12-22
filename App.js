0fe7ebd2aded7540e1b69e4a9035a97a40cacde7
LakGillJPN <lakgill88@gmail.com>
2023-12-22 18:20:36 +0900

    feat: add nxt logo

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Image
+          source={require('./assets/nxt_logo.png')}
+          style={{ width: 120, height: 50 }}
+          resizeMode='contain'>
+             </Image>
+      </View>
+      <Text style={styles.title}>Name Generator</Text>
+      <StatusBar style="auto"/>
+      <View style={styles.birthday}>
+        <TextInput style={styles.input} />
+      </View>
     </View>
   );
 }
@@ -13,8 +24,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'gold',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  birthday: {
+    backgroundColor: 'white'
+  },
+  input: {
+    height: 50,
+    width: 300,
+    borderColor: 'gray',
+    borderWidth: 1,
   },
 });
